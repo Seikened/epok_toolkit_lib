2c3a6ddd5a67e3365d07ce67d9cd85ab2af58839
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-07-10 17:45:47 -0600

    Initial commit: epok-toolkit v0.1.0

---
+++ tag.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Define la versión automáticamente (puedes ajustarla o obtenerla de otra fuente)
+VERSION="v0.1.0"
+
+echo "Creando una nueva etiqueta: $VERSION"
+git tag "$VERSION"
+
+echo "Subiendo la etiqueta al repositorio remoto"
+git push origin "$VERSION"
