c17743d257d86398588bbd7cae823a61beb2e211
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-07-11 13:20:58 -0600

    bug fifed to 1.3.0

--- tag.sh
+++ tag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define la versión automáticamente (puedes ajustarla o obtenerla de otra fuente)
-VERSION="v1.2.0"
+VERSION="v1.3.0"
 
 echo "Creando una nueva etiqueta: $VERSION"
 git tag "$VERSION"
