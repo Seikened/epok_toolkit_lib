74cf07f845115df17ef90ec85996f8a295c195c2
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-07-10 18:01:32 -0600

    Change file mode of tag.sh to make it executable

--- tag.sh
+++ tag.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Define la versión automáticamente (puedes ajustarla o obtenerla de otra fuente)
-VERSION="v0.1.0"
+VERSION="v0.2.0"
 
 echo "Creando una nueva etiqueta: $VERSION"
 git tag "$VERSION"
