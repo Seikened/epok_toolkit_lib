16931c9f9ec607906b7a52b0a069dd953cd65516
Fernando Leon Franco <fernanlee2131@gmail.com>
2025-07-21 20:55:33 -0600

    Bump version to 1.11.1 in pyproject.toml and tag.sh; comment out unused methods in BaseOptimizedViewSet

--- epok_toolkit/django/viewsets.py
+++ epok_toolkit/django/viewsets.py
@@ -18,24 +18,24 @@ class BaseOptimizedViewSet(viewsets.ModelViewSet):
     simple_serializer_class = None
     full_serializer_class = None
 
-    def get_queryset(self):        
-        qs = super().get_queryset()
+    # def get_queryset(self):        
+    #     qs = super().get_queryset()
         
-        if not (hasattr(qs, 'full') and hasattr(qs, 'simple')):
-            raise NotImplementedError(
-            f"❌ El modelo {qs.model.__name__} no está usando un OptimizedManager con full() y simple()."
-        )
+    #     if not (hasattr(qs, 'full') and hasattr(qs, 'simple')):
+    #         raise NotImplementedError(
+    #         f"❌ El modelo {qs.model.__name__} no está usando un OptimizedManager con full() y simple()."
+    #     )
 
-        if self.action == 'list':
-            return qs.simple()
-        elif self.action in ['retrieve', 'update', 'partial_update']:
-            return qs.full()
-        return qs
+    #     if self.action == 'list':
+    #         return qs.simple()
+    #     elif self.action in ['retrieve', 'update', 'partial_update']:
+    #         return qs.full()
+    #     return qs
 
-    def get_serializer_class(self):
-        # Cambia el serializer según la acción
-        if self.action == 'list' and self.simple_serializer_class:
-            return self.simple_serializer_class
-        if self.action in ['retrieve', 'update', 'partial_update'] and self.full_serializer_class:
-            return self.full_serializer_class
-        return super().get_serializer_class()
+    # def get_serializer_class(self):
+    #     # Cambia el serializer según la acción
+    #     if self.action == 'list' and self.simple_serializer_class:
+    #         return self.simple_serializer_class
+    #     if self.action in ['retrieve', 'update', 'partial_update'] and self.full_serializer_class:
+    #         return self.full_serializer_class
+    #     return super().get_serializer_class()
